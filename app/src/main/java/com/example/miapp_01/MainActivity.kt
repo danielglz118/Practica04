335d3fc5c4152141bea125db9551e56a770b88f3
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-08-30 20:54:54 -0600

    primer commit

--- app/src/main/java/com/example/miapp_01/MainActivity.kt
+++ app/src/main/java/com/example/miapp_01/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.miapp_01.ui.theme.MiAPP_01Theme
 
@@ -26,23 +32,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiAPP_01Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  saludo("Jose Luis")
+                    Formulario()
                 }
             }
         }
     }
 }
-
 @Composable
-fun saludo(nombre: String){
-    Text(
-        text=nombre,
-        fontSize = 50.sp,
+fun Formulario(){
+    Column(
         modifier= Modifier
-            .padding(106.dp)
-            .background(
-                Color.Cyan,
-                shape= RoundedCornerShape(12.dp)
-            )
-    )
+            .fillMaxWidth()
+            .padding(96.dp),
+        verticalArrangement =
+            Arrangement.spacedBy(12.dp),
+        horizontalAlignment=
+            Alignment.CenterHorizontally
+    ){
+        Text("Titulo",fontSize=56.sp)
+        Text("Subtitulo",fontSize=34.sp)
+        Button(onClick = {}) {
+            Text("Ingresar", fontWeight = FontWeight.Bold)
+        }
+    }
 }
