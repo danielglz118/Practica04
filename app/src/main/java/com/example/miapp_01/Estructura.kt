8b88f1fe9429a0d0815493ad11ef918ee25cb4a9
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-08-30 21:03:53 -0600

    primer commit

---
+++ app/src/main/java/com/example/miapp_01/Estructura.kt
@@ -0,0 +1,8 @@
+package com.example.miapp_01
+
+import android.graphics.Color
+
+data class Estructura(
+    val nombre: String,
+    val color: Color
+)
--- app/src/main/java/com/example/miapp_01/MainActivity.kt
+++ app/src/main/java/com/example/miapp_01/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -23,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.miapp_01.ui.theme.MiAPP_01Theme
 
@@ -41,19 +45,26 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Formulario(){
-    Row(
-        modifier= Modifier
-            .fillMaxWidth()
-            .padding(96.dp),
-        horizontalArrangement  =
-            Arrangement.spacedBy(12.dp),
-        verticalAlignment =
-            Alignment.CenterVertically
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
     ){
-        Text("Titulo",fontSize=16.sp)
-        Text("Subtitulo",fontSize=14.sp)
-        Button(onClick = {}) {
-            Text("Ingresar", fontWeight = FontWeight.Bold)
-        }
+
     }
+}
+@Composable
+fun Espacio(espacio: Int){
+    Spacer(modifier=Modifier.padding(espacio.dp))
+}
+@Composable
+fun Texto(texto:String,color:Color){
+    Text(
+        text=texto,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color= color,
+        textAlign = TextAlign.Center,
+        modifier= Modifier
+            .background(Color.Black)
+            .fillMaxWidth()
+    )
 }
