1f20c849142571302933601dab3558b4308e9b2e
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-09-03 14:01:43 -0600

    Primera practica

--- app/src/main/java/com/example/miapp_01/MainActivity.kt
+++ app/src/main/java/com/example/miapp_01/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.miapp_01
 
+import android.R.attr.button
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -12,19 +16,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.miapp_01.ui.theme.MiAPP_01Theme
@@ -32,71 +46,54 @@ import com.example.miapp_01.ui.theme.MiAPP_01Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             MiAPP_01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Formulario()
-                }
+                Formulario()
             }
         }
     }
 }
-@Composable
-fun Formulario(){
-    val listaBotones=listOf(
-        Estructura("Rojo",Color.Red),
-        Estructura("Verde",Color.Green),
-        Estructura("Amarillo",Color.Yellow),
-        Estructura("Azul",Color.Blue),
-        Estructura("Cyan",Color.Cyan),
-        Estructura("Dark Grey",Color.DarkGray),
-        Estructura("Rojo",Color.Red),
-        Estructura("Verde",Color.Green),
-        Estructura("Amarillo",Color.Yellow),
-        Estructura("Azul",Color.Blue),
-        Estructura("Cyan",Color.Cyan),
-        Estructura("Dark Grey",Color.DarkGray)
 
-    )
+@Composable
+fun Formulario() {
+
+    var contador by remember { mutableStateOf(0) }
+
     Column(
-        modifier=Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        horizontalAlignment=
-            Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-    ){
-        Text("Bienvenido",
-            color=Color.Red,
-            fontWeight = FontWeight.Bold,
-            fontSize = 40.sp,
-            textAlign= TextAlign.Center,
-            modifier=Modifier
-                .background(Color.Black)
-                .padding(horizontal = 20.dp)
-                .fillMaxWidth()
-
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Text("Jetpack")
-        Spacer(modifier = Modifier.height(10.dp))
-        Text("Compose")
-        LazyRow(  contentPadding = PaddingValues(8.dp)){
-            items(listaBotones){estructura->
-                Botones(estructura)
-            }
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .background(Color.Blue, CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = contador.toString(),
+                fontSize = 48.sp,
+                color = Color.White
+            )
         }
-    }
-}
 
-@Composable
-fun Botones(estructura:Estructura){
-    Button(
-        onClick = {},
-        modifier = Modifier
-            .background(estructura.color)
-    ){
-        Text(text=estructura.nombre)
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .width(200.dp)
+                .height(60.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF536A99)
+            )
+        ) {
+            Text(
+                "Sumar",
+                fontSize = 24.sp
+            )
+        }
     }
 }
