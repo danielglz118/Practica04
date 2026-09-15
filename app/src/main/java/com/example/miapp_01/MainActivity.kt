659391677943daf69d5c241c91295936e250a011
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-09-15 13:47:58 -0600

    1

--- app/src/main/java/com/example/miapp_01/MainActivity.kt
+++ app/src/main/java/com/example/miapp_01/MainActivity.kt
@@ -1,50 +1,31 @@
 package com.example.miapp_01
 
-import android.R.attr.button
-import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.miapp_01.ui.theme.MiAPP_01Theme
 
 class MainActivity : ComponentActivity() {
@@ -54,31 +35,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiAPP_01Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Screen()
+                    Pantalla()
                 }
             }
         }
     }
 
 
+    @Preview(showBackground = true)
     @Composable
-    fun Screen() {
+    fun Pantalla() {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = "Prueba",
-                    tint = Color.Red,
-                    modifier = Modifier.size(50.dp)
-                )
-
-                Spacer(modifier = Modifier.height(40.dp))
-                Text("Favorito", fontSize = 30.sp)
-            }
+            Image(
+                painter = painterResource(R.drawable.santos),
+                contentDescription = "Los mejores",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(300.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Image(
+                imageVector= ImageVector.vectorResource(R.drawable.santos_logo_1),
+                contentDescription = "The Best",
+                contentScale= ContentScale.Fit,
+                modifier = Modifier.size(300.dp)
+            )
         }
     }
 }
