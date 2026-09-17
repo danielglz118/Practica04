428b050f08e2cac3f5553f8f8aa72a891791262d
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-09-17 13:51:49 -0600

    OSWALDO GOD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/miapp_01/MainActivity.kt
+++ app/src/main/java/com/example/miapp_01/MainActivity.kt
@@ -26,7 +26,34 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImage
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import com.example.miapp_01.ui.theme.MiAPP_01Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,36 +61,93 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MiAPP_01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Pantalla()
+                Scaffold(modifier = Modifier.fillMaxSize()) { margen ->
+                    PeliAnimadasPantalla(modifier = Modifier.padding(margen))
                 }
             }
         }
     }
 
+        @Composable
+        fun PeliAnimadasPantalla(modifier: Modifier = Modifier) {
 
-    @Preview(showBackground = true)
-    @Composable
-    fun Pantalla() {
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Image(
-                painter = painterResource(R.drawable.santos),
-                contentDescription = "Los mejores",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.size(300.dp)
+            val PelAnimadas = listOf(
+                Triple("Pelicula de Carreras", "Animación. 120 min.", R.drawable.cars),
+                Triple("Pelicula del Dia de Muertos", "Animación.", R.drawable.coco),
+                Triple(
+                    "Pelicula de historia de ogro",
+                    "Animación/Adulta. 120 min.",
+                    R.drawable.shrek
+                ),
+                Triple("Pelicula de SuperHeroes", "Animación.", R.drawable.increibles),
+                Triple("Pelicula de juguetes", "Animación. 110 min.", R.drawable.toystory),
+                Triple("Pelicula de Robot en el Futuro", "Animación", R.drawable.walle)
             )
-            Spacer(modifier = Modifier.width(20.dp))
-            Image(
-                imageVector= ImageVector.vectorResource(R.drawable.santos_logo_1),
-                contentDescription = "The Best",
-                contentScale= ContentScale.Fit,
-                modifier = Modifier.size(300.dp)
-            )
+
+            LazyColumn(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+
+                item {
+                    Text(
+                        text = "PELICULA DE CINE ANIMADAS POPOULARES DEL SIGLO XX1",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp)
+                    )
+                }
+
+                items(PelAnimadas) { pelicula ->
+
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.Blue
+                        )
+                    ) {
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            Image(
+                                painter = painterResource(pelicula.third),
+                                contentDescription = "",
+                                modifier = Modifier.size(90.dp),
+                                contentScale = ContentScale.Crop
+                            )
+
+                            Spacer(modifier = Modifier.width(16.dp))
+
+                            Column {
+
+                                Text(
+                                    text = pelicula.first,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.White
+                                )
+
+                                Spacer(modifier = Modifier.height(8.dp))
+
+                                Text(
+                                    text = pelicula.second,
+                                    fontSize = 14.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-}
+
