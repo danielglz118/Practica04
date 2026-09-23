777f858a7becb0dab4896a0f3f20949859887e11
danielglz118 <alu.22130614@correo.itlalaguna.edu.mx>
2026-09-23 08:28:05 -0600

    1 paso Completo de Vistas

--- app/src/main/java/Views/ContadorView.kt
+++ app/src/main/java/Views/ContadorView.kt
@@ -8,7 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Contadorview(view: contadorViewModel){
